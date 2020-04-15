a3145ee3595e08c81e5919b3e11cb386d84dd0a5
Souriya Phongsavanh <souriya.phongsavanh777@gmail.com>
2020-04-15 14:07:13 +0200

    add our basic views (served by vue-router)

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: { name: 'albums' }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/albums',
+    name: 'albums',
+    component: () => import('../views/Albums')
+  },
+  {
+    path: '/playlists',
+    name: 'playlists',
+    component: () => import('../views/Playlists')
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import('../views/Search')
   }
 ]
 
---
+++ src/views/Playlists.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Playlists</h1>
+</template>
